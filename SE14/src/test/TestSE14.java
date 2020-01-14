f0e8c6301e3ba25c3d4839da36f7e0a063f12bf3
Your Name <eteranimal@protonmail.com>
2020-01-14 09:51:05 +0100

    Test hinzugefügt

---
+++ SE14/src/test/TestSE14.java
@@ -0,0 +1,41 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ctr.Datei;
+import ctr.Info;
+import ctr.Ordner;
+
+public class TestSE14 {
+
+	@Test
+	public void test() {
+		
+		Ordner testOrdner = new Ordner("Test Ordner");
+		Datei testDatei = new Datei("Test", ".test");
+		
+		testOrdner.add(testDatei);
+		
+		Ordner testUnterOrdner = new Ordner("TestUnterOrdner");
+		Datei testUnterDatei = new Datei("TestUnterDatei", ".test");
+		
+		Ordner testLeererOrdner = new Ordner("LeererOrdner");
+		
+		testOrdner.add(testLeererOrdner);
+		
+		testUnterOrdner.add(testUnterDatei);
+		testOrdner.add(testUnterOrdner);
+		
+		Info infoTest = new Info();
+		
+		infoTest.visit(testOrdner);
+		System.out.println(infoTest.getTiefe());
+		System.out.println(infoTest.getBlatt());
+	
+		assertEquals(2, infoTest.getTiefe());
+		assertEquals(4,infoTest.getBlatt());
+		
+	}
+}
